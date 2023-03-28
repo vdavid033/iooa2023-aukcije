254a8607ec775691849f200ddbece92b771e6c89
Emil <ekrulcic@symmetria-consulting.hr>
2023-03-28 23:31:09 +0200

    #team2 - Maknute nepotrebne stranice

--- aukcije/src/layouts/MainLayout.vue
+++ aukcije/src/layouts/MainLayout.vue
@@ -13,7 +13,6 @@
 
         <q-toolbar-title> Aplikacija za aukcije </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -51,36 +50,6 @@ const linksList = [
     caption: "github.com/iooa-aukcije",
     icon: "code",
     link: "https://github.com/vdavid033/iooa2023-aukcije",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
   },
 ];
 
