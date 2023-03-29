c24c4fa34b75c6c85ed5b2a557104355e6689638
iburic97 <iburic@veleri.hr>
2023-03-29 14:09:58 +0200

    korekcija routa i linkova

--- aukcije/src/layouts/MainLayout.vue
+++ aukcije/src/layouts/MainLayout.vue
@@ -12,24 +12,25 @@
         />
 
         <q-toolbar-title> Aplikacija za aukcije </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header class="text-bold text-black"> Mogućnosti </q-item-label>
+        <q-item-label header class="text-bold text-black">
+          Mogućnosti
+        </q-item-label>
 
-<!--        Za navigaciju bez otvaranja novog tab-a-->
+        <!--        Za navigaciju bez otvaranja novog tab-a-->
         <q-btn>
           <router-link to="/" class="link-style">Početna stranica</router-link>
         </q-btn>
 
         <q-btn>
-          <router-link to="postavi" class="link-style">Dodaj aukciju</router-link>
+          <router-link to="postavi" class="link-style"
+            >Dodaj aukciju</router-link
+          >
         </q-btn>
-
-        
       </q-list>
     </q-drawer>
 
@@ -42,31 +43,13 @@
 <script>
 import { defineComponent, ref } from "vue";
 
-const linksList = [
-  {
-    title: "Postavi aukciju",
-    caption: "Unos aukcije",
-    icon: "code",
-    link: "postavi",
-  },
-  {
-    title: "Github Aukcije",
-    caption: "github.com/iooa-aukcije",
-    icon: "code",
-    link: "https://github.com/vdavid033/iooa2023-aukcije",
-  },
-];
-
 export default defineComponent({
   name: "MainLayout",
-
-  
 
   setup() {
     const leftDrawerOpen = ref(false);
 
     return {
-     
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
