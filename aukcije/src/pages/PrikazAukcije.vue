18c426b3b5b4327ee697bfa7b102ac906f998678
Emil <ekrulcic@symmetria-consulting.hr>
2023-04-22 22:55:50 +0200

    #team2 - maknut slide show i prikazana slika odabranog predmeta

--- aukcije/src/pages/PrikazAukcije.vue
+++ aukcije/src/pages/PrikazAukcije.vue
@@ -2,7 +2,7 @@
   <q-card class="q-pa-sm q-gutter-sm" flat bordered>
     <q-card-section>
       <div class="text-h3 text-bold text-center text-blue-7 q-ml-sm">
-        Prikaz aukcije {{ this.sifra_predmeta }}
+        Prikaz aukcije
         <div class="q-ml-sm flex justify-end q-gutter-sm">
           <q-btn
             size="15px"
@@ -47,72 +47,7 @@
       <div class="row flex flex-center">
         <div style="width: 600px">
           <q-card>
-            <q-carousel
-              v-model="slide"
-              transition-prev="slide-right"
-              transition-next="slide-left"
-              swipeable
-              animated
-              :autoplay="autoplay"
-              control-color="green"
-              navigation
-              padding
-              ref="carousel"
-              infinite
-              arrows
-              height="350px"
-              class="bg-white shadow-2 rounded-borders"
-            >
-              <q-carousel-slide :name="2" class="column no-wrap">
-                <div
-                  class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-                >
-                  <q-img
-                    class="rounded-borders col-6 full-height"
-                    src="https://cdn.quasar.dev/img/material.png"
-                  >
-                  </q-img>
-                  <q-img
-                    class="rounded-borders col-6 full-height"
-                    src="https://cdn.quasar.dev/img/material.png"
-                  >
-                  </q-img>
-                </div>
-              </q-carousel-slide>
-              <q-carousel-slide :name="3" class="column no-wrap">
-                <div
-                  class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-                >
-                  <q-img
-                    class="rounded-borders col-6 full-height"
-                    src="https://cdn.quasar.dev/img/material.png"
-                  >
-                  </q-img>
-                  <q-img
-                    class="rounded-borders col-6 full-height"
-                    src="https://cdn.quasar.dev/img/material.png"
-                  >
-                  </q-img>
-                </div>
-              </q-carousel-slide>
-
-              <template v-slot:control>
-                <q-carousel-control
-                  position="top-right"
-                  :offset="[18, 18]"
-                  class="text-white rounded-borders"
-                  style="background: rgba(0, 0, 0, 0.3); padding: 4px 8px"
-                >
-                  <q-toggle
-                    dense
-                    dark
-                    color="orange"
-                    v-model="autoplay"
-                    label="Auto Play"
-                  />
-                </q-carousel-control>
-              </template>
-            </q-carousel>
+            <q-img :src="item.slika" no-native-menu />
           </q-card>
         </div>
       </div>
