b347db09f16eb16e5829a452a1aa90aa4fd5d959
iburic97 <iburic@veleri.hr>
2023-04-26 14:03:14 +0200

    dodan app.post(/api/unos-slike) za spremanje linka slike pretvorene u base64 formatu u bazu

--- aukcije-server/index.js
+++ aukcije-server/index.js
@@ -1,66 +1,106 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
-const {join} = require("path");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const { join } = require("path");
+const path = require("path");
 
-const app = express()
-const port = 3306
+const app = express();
+const port = 3306;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors({origin: '*'}))
+// Parser za JSON podatke
+app.use(bodyParser.json());
+
+// Parser za podatke iz formi
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Postavke direktorija za statičke datoteke
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "*" }));
 
 const connection = mysql.createConnection({
-    host: 'student.veleri.hr',
-    user: 'iooa-aukcije',
-    password: '11',
-    database: 'iooa-aukcije'
+  host: "student.veleri.hr",
+  user: "iooa-aukcije",
+  password: "11",
+  database: "iooa-aukcije",
 });
 
-connection.connect()
+connection.connect();
 
-app.get('/api/all-korisnik', (req, res) => {
+app.get("/api/all-korisnik", (req, res) => {
+  connection.query("SELECT * FROM korisnik", (error, results) => {
+    if (error) throw error;
 
-    connection.query('SELECT * FROM korisnik', (error, results) => {
-        if (error) throw error;
-
-        res.send(results)
-    })
-})
-
-app.get('/api/all-predmet', (req, res) => {
-
-    connection.query('SELECT sifra_predmeta, naziv_predmeta, slika, pocetna_cijena, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, vrijeme_pocetka, vrijeme_zavrsetka)), \'%H:%i:%s\' ) AS preostalo_vrijeme, svrha_donacije, opis_predmeta FROM predmet ORDER BY preostalo_vrijeme DESC\n', (error, results) => {
-        if (error) throw error;
-
-        res.send(results)
-    })
-})
-
-app.get('/api/get-predmet/:id', (req, res) => {
-    const { id } = req.params;
-
-    connection.query('SELECT naziv_predmeta, sifra_predmeta,  slika, pocetna_cijena, vrijeme_pocetka, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, vrijeme_pocetka, vrijeme_zavrsetka)), \'%H:%i:%s\' ) AS preostalo_vrijeme, svrha_donacije, opis_predmeta FROM predmet WHERE sifra_predmeta = ?', [id], (error, results) => {
-        if (error) throw error;
-        res.send(results)
-    });
+    res.send(results);
+  });
 });
-app.get('/api/get-kategorija-predmet/:id', (req, res) => {
-    const { id } = req.params;
 
-    connection.query('SELECT * FROM kategorija JOIN predmet ON kategorija.sifra_kategorije = predmet.sifra_kategorije WHERE kategorija.sifra_kategorije = ?', [id], (error, results) => {
-        if (error) throw error;
-        res.send(results)
-    });
+app.get("/api/all-predmet", (req, res) => {
+  connection.query(
+    "SELECT sifra_predmeta, naziv_predmeta, slika, pocetna_cijena, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, vrijeme_pocetka, vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, svrha_donacije, opis_predmeta FROM predmet ORDER BY preostalo_vrijeme DESC\n",
+    (error, results) => {
+      if (error) throw error;
+
+      res.send(results);
+    }
+  );
 });
-app.get('/api/all-kategorija', (req, res) => {
-    connection.query('SELECT * FROM kategorija', (error, results) => {
-        if (error) throw error;
-        res.send(results)
-    })
-})
+
+app.get("/api/get-predmet/:id", (req, res) => {
+  const { id } = req.params;
+
+  connection.query(
+    "SELECT naziv_predmeta, sifra_predmeta,  slika, pocetna_cijena, vrijeme_pocetka, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, vrijeme_pocetka, vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, svrha_donacije, opis_predmeta FROM predmet WHERE sifra_predmeta = ?",
+    [id],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.get("/api/get-kategorija-predmet/:id", (req, res) => {
+  const { id } = req.params;
+
+  connection.query(
+    "SELECT * FROM kategorija JOIN predmet ON kategorija.sifra_kategorije = predmet.sifra_kategorije WHERE kategorija.sifra_kategorije = ?",
+    [id],
+    (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.get("/api/all-kategorija", (req, res) => {
+  connection.query("SELECT * FROM kategorija", (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/api/unos-slike", function (req, res) {
+  const data = req.body;
+  const slika = data.slika;
+
+  connection.query(
+    "INSERT INTO predmet (slika) VALUES (?)",
+    [slika],
+    function (error, results, fields) {
+      if (error) {
+        console.error(error);
+        return res.status(500).send({
+          error: true,
+          message: "Dogodila se greška prilikom dodavanja teksta.",
+        });
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "Slika je dodana.",
+      });
+    }
+  );
+});
 
 app.listen(port, () => {
-    console.log(`Server running at port: ${port}`)
-})
+  console.log(`Server running at port: ${port}`);
+});
