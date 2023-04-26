765d94ae72920be4bc4ace5d2f58fc50ede908b7
MatteoK07 <mkutnjak@veleri.hr>
2023-04-26 17:47:14 +0200

    Spojeno postavljanje i prikaz aukcija u index.js

--- aukcije-server/index.js
+++ aukcije-server/index.js
@@ -47,6 +47,16 @@ app.get('/api/get-predmet/:id', (req, res) => {
     });
 });
 
+app.post('/unosPredmeta', function (request, response) {
+    const data = request.body;
+    predmet = [[data.sifra_predmeta, data.naziv_predmeta,  data.opis_predmeta, data.slika, data.vrijeme_pocetka, data.vrijeme_zavrsetka, data.pocetna_cijena, data.svrha_donacije, data.id_korisnika, data.sifra_kategorije]]
+    connection.query('INSERT INTO predmet (sifra_predmeta, naziv_predmeta,  opis_predmeta, slika, vrijeme_pocetka, vrijeme_zavrsetka, pocetna_cijena, svrha_donacije, id_korisnika, sifra_kategorije) VALUES ?', 
+    [predmet], function (error, results, fields) {
+      if (error) throw error;
+      return response.send({ error: false, data: results, message: 'Predmet je dodan.' });
+    });
+  });
+  
 
 app.listen(port, () => {
     console.log(`Server running at port: ${port}`)
