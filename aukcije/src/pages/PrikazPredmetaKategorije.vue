f8461610b4db980ea7a61339c9f1ded5a3fb052f
rbenic0407 <rbenic@veleri.hr>
2023-04-28 11:15:57 +0200

    prikazivanje kategorije i predmeta za odredenu kategoriju

--- aukcije/src/pages/PrikazPredmetaKategorije.vue
+++ aukcije/src/pages/PrikazPredmetaKategorije.vue
@@ -1,99 +1,81 @@
 <template>
 
-    <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px"
-      >{{kategorija.naziv_kategorije}}</q-item
-    >
-    <div class="q-pa-sm row flex flex-center">
-      <div
+  <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px"
+  ></q-item
+  >
+  <div class="q-pa-sm row flex flex-center">
+    <div
         v-for="item in items"
         :key="item.sifra_predmeta"
         class="q-pa-md"
         style="width: 400px"
-      >
-      
-        <q-card @click = "navigateToItem(item.sifra_predmeta)">
-          <q-img :src="item.slika" no-native-menu />
-          <q-item-section>
-            
-            <q-item class="q-pa-sm text-bold text-blue-7">{{
+    >
+
+      <q-card @click = "navigateToItem(item.sifra_predmeta)">
+        <q-img :src="item.slika" no-native-menu />
+        <q-item-section>
+
+          <q-item class="q-pa-sm text-bold text-blue-7">{{
               item.naziv_predmeta
             }}</q-item>
-            <q-item>Početna cijena: {{ item.pocetna_cijena }}$</q-item>
-            <q-item>Vrijeme zavrsetka: {{ formattedDate(item.vrijeme_zavrsetka) }}</q-item>
-            <q-item
-              >Preostalo vrijeme aukcije: {{ item.preostalo_vrijeme }}</q-item
-            >
-          </q-item-section>
-        </q-card>
-      </div>
+          <q-item>Početna cijena: {{ item.pocetna_cijena }}$</q-item>
+          <q-item>Vrijeme zavrsetka: {{ formattedDate(item.vrijeme_zavrsetka) }}</q-item>
+          <q-item
+          >Preostalo vrijeme aukcije: {{ item.preostalo_vrijeme }}</q-item
+          >
+        </q-item-section>
+      </q-card>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from "vue";
-  import axios from "axios";
-  
-  const baseUrl = "http://localhost:3306/api/";
-  
-  export default {
-    computed: {
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3306/api/";
+
+export default {
+  computed: {
     sifra_kategorije() {
       return this.$route.query.sifra_kategorije;
     },
-    sifra_predmeta() {
-      return this.$route.query.sifra_predmeta;
-    }
   },
-    setup() {
-      return {
-        date: ref("2023-03-27 12:44"),
+  setup() {
+    return {
+      date: ref("2023-03-27 12:44"),
       date2: ref("2023-03-27 12:44"),
       dialog: ref(false),
       small: ref(false),
       model: ref(null),
-      };
-    },
-    data() {
-      return {
-        kategorija: [],
-        items:[],
-      };
-    },
-    mounted() {
-      axios
+    };
+  },
+  data() {
+    return {
+      items:[],
+    };
+  },
+  mounted() {
+    axios
         .get(baseUrl + "get-kategorija-predmet/" + this.sifra_kategorije, {})
         .then((response) => {
-      this.kategorija = response.data[0];
-    });
-    axios
-        .get(baseUrl + "get-predmet/" + this.sifra_predmeta, {})
-        .then((response) => {
-      this.items = response.data[0];
-    });
-    },
-  
-    methods: {
-      formattedDate(dateString) {
-        return new Date(dateString).toLocaleString('hr-HR').replace(',', '');
-      },
-      navigateToItem (sifra_predmeta){
-      this.$router.push({ path: 'prikaz', query: { sifra_predmeta } });
-         },
-      
-           navigateToItem1 (sifra_kategorije)
-           {this.$router.push({ path: 'kategorija', query: { sifra_kategorije } });
-    }
+          this.items = response.data;
+        });
   },
-  
-  };
-  </script>
-  <style>
-  /*.q-carousel {
-       height: 300px;
-       max-width: 500px;*/
-  .q-img {
-    height: 300px;
-    max-width: 500px;
+
+  methods: {
+    formattedDate(dateString) {
+      return new Date(dateString).toLocaleString('hr-HR').replace(',', '');
+    },
+    navigateToItem (sifra_predmeta){
+      this.$router.push({ path: 'prikaz', query: { sifra_predmeta } });
+    },
   }
-  </style>
-  
+};
+</script>
+<style>
+.q-img {
+  height: 300px;
+  max-width: 500px;
+}
+</style>
